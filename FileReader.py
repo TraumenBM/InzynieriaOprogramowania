a1bf4321281c7c3cda1592bf2c93fe998d8a14be
Iga Kaczmarczyk <kontakt@igakaczmarczyk.pl>
2019-11-12 11:38:39 +0100

    Fixed regular expression

--- FileReader.py
+++ FileReader.py
@@ -52,7 +52,7 @@ class FileReader:
     def checkFilesExistance(self, files, dependencies):
         existedFiles = []
         for dep in dependencies:
-            dep = re.sub(r'[\\|/]', os.path.sep, dep, 0)
+            dep = re.sub(r"[\\|/]", os.path.sep, dep, 0)
             depPath = os.path.abspath(dep)
             # print(depPath)
             if depPath in files:
